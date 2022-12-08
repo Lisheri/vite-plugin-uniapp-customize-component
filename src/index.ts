73715ac5ca6a4ccbc8b0d4f27e2984a9f180ff82
Lisher <496585582@qq.com>
2022-12-08 15:33:32 +0800

    bug-fix: 修复组件内部使用小程序原生自定义组件无法接收参数问题

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,60 @@
-import { Options } from './uniappCustomizeComponent';
-const UniappCustomizeComponent = (options: Options = {}) => {
+type UsingComponents = Record<string, string>;
+type Options = Record<string, UsingComponents>;
+
+type Platform = "wx" | "tt";
+
+// 插件本体
+const UniappCustomizeComponent = (
+  options: Options = {},
+  platform: Platform = "wx"
+) => {
   options = Object.keys(options).reduce((prev: Options, item: string) => {
     return {
       ...prev,
-      [`${item}.json`]: options[item]
-    }
+      [`${item}.json`]: options[item],
+      [`${item}.${platform}ml`]: options[item],
+    };
   }, {});
-  const name: string = 'vite-plugin-vue-uniapp-customize-component';
+  const name: string = "vite-plugin-vue-uniapp-customize-component";
   return {
     name,
     // 在文件写入结束时调用
-    generateBundle(_code: any, bundle: any) {
-      Object.keys(bundle).forEach(name => {
-        if (Object.keys(options).includes(name)) {
-          const usingComponentsOption = options[name];
-          const appJson = JSON.parse(bundle[name].source)
-          appJson.usingComponents = {
-            ...(appJson.usingComponents || {}),
-            ...usingComponentsOption
+    generateBundle(_options: any, bundle: any) {
+      Object.keys(bundle).forEach((bundleName) => {
+        if (Object.keys(options).includes(bundleName)) {
+          const currentBundle = bundle[bundleName];
+          if (bundleName.includes("json")) {
+            // 处理配置文件
+            const usingComponentsOption = options[bundleName];
+            const appJson = JSON.parse(currentBundle.source);
+            appJson.usingComponents = {
+              ...(appJson.usingComponents || {}),
+              ...usingComponentsOption,
+            };
+            currentBundle.source = JSON.stringify(appJson);
+          } else {
+            // 处理组件标签属性
+            const tags = Object.keys(options[bundleName]);
+            tags.forEach((tag) => {
+              const reg = new RegExp(`\\<${tag}[\\s\\S]*?</${tag}>`, "g");
+              currentBundle.source = currentBundle.source.replace(
+                reg,
+                (str: string) => {
+                  if (!str.includes('u-t="m"')) {
+                    // 构建出来没有u-t="m"属性
+                    const targetProps = 'u-t="m"';
+                    const resourcePropsArr = str.split(" ");
+                    resourcePropsArr.splice(2, 0, targetProps);
+                    return resourcePropsArr.join(" ");
+                  }
+                  return str;
+                }
+              );
+            });
           }
-          bundle[name].source = JSON.stringify(appJson);
         }
-      })
-    }
+      });
+    },
   };
 };
 export default UniappCustomizeComponent;
